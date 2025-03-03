7cfb7619c555fb5b2d40aa200868ce798c8b1b05
sin <shulgin.grigoriy@yandex.ru>
2025-03-03 16:47:33 +0300

    updating new publish commands

--- build_and_upload_large_size.sh
+++ build_and_upload_large_size.sh
@@ -8,4 +8,6 @@ docker save iisdc-admin iisdc-frontend > admin_and_frontend.tar
 
 scp admin_and_frontend.tar $SERVER:/opt/IISDC/admin_and_frontend.tar
 
+ssh $SERVER "cd /opt/IISDC; docker load -i admin_and_frontend.tar"
 
+ssh $SERVER "cd /opt/IISDC; docker compose up -d"
